290bca7ba960b51c9ab1165db4bc2337144e3034
Nitish Kumar <nitishkumar971727@gmail.com>
2026-06-18 08:13:46 +0530

    commit

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ from sqlalchemy import Integer
 from sqlalchemy import String
 from sqlalchemy import TIMESTAMP
 from sqlalchemy.sql import func
+from sqlalchemy import Column, Integer, String, Boolean
 
 from database import Base
 
@@ -16,4 +17,25 @@ class Admin(Base):
     created_at = Column(
         TIMESTAMP,
         server_default=func.now()
+    )
+
+from sqlalchemy import Column, Integer, String, Boolean
+# models.py
+
+from sqlalchemy import Column, Integer, String, Boolean
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100))
+    email = Column(String(100), unique=True)
+
+    is_blocked = Column(Boolean, default=False)
+    is_active = Column(Boolean, default=True)
+
+    verification_status = Column(
+        String(20),
+        default="Pending"
     )
